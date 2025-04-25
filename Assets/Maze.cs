81ab2c729875e6d4ea07b893ef296247e8eea76d
Brian Teddy <teddybrian543@gmail.com>
2025-04-25 12:25:23 +0300

    Change Generate function

--- Assets/Maze.cs
+++ Assets/Maze.cs
@@ -26,7 +26,7 @@ public class Maze : MonoBehaviour
             }
     }
 
-    void Generate()
+    public virtual void Generate()
     {
         for (int z = 0; z < depth; z++)
             for (int x = 0; x < width; x++)
