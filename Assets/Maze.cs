5f3a6751ec14161bc06ac5e79fbac36d52ecb385
Brian Teddy <teddybrian543@gmail.com>
2025-04-25 10:21:47 +0300

    Initial Commit

---
+++ Assets/Maze.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    public int width = 30; //x length
+    public int depth = 30; //z length
+    public byte[,] map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialiseMap();
+        Generate();
+        DrawMap();
+    }
+
+    void InitialiseMap()
+    {
+        map = new byte[width,depth];
+        for (int z = 0; z < depth; z++)
+            for (int x = 0; x < width; x++)
+            {
+                    map[x, z] = 1;     //1 = wall  0 = corridor
+            }
+    }
+
+    void Generate()
+    {
+        for (int z = 0; z < depth; z++)
+            for (int x = 0; x < width; x++)
+            {
+               if(Random.Range(0,100) < 50)
+                 map[x, z] = 0;     //1 = wall  0 = corridor
+            }
+    }
+
+    void DrawMap()
+    {
+        for (int z = 0; z < depth; z++)
+            for (int x = 0; x < width; x++)
+            {
+                if (map[x, z] == 1)
+                {
+                    Vector3 pos = new Vector3(x, 0, z);
+                    GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    wall.transform.position = pos;
+                }
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
